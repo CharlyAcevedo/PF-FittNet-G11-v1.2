cd7947a704a9b71581d7ac4827f2de50b19c022a
Esteban5957L <4estebanvillalba20@gmail.com>
2022-06-06 12:49:20 -0300

    detail gym

--- client/src/components/GymDetail/GymDetail.jsx
+++ client/src/components/GymDetail/GymDetail.jsx
@@ -8,64 +8,81 @@ import { useNavigate } from "react-router-dom";
 import { NavBar3 } from "./NavBar3";
 import CartItem from "../CartItem/CartItem";
 
-
+import style from "./styles/style.module.css";
+import user from "../../asets/icons/users.svg";
+import actividades from "../../asets/icons/trending-up.svg";
+import start from "../../asets/icons/star.svg";
+import map from "../../asets/icons/map-pin.svg";
+import { CardIcons } from "../../helpers/Cards/Cards";
 
 export default function GymDetail() {
-    let { userId } = useParams();
-    
-    const navigate = useNavigate(); 
+  let { userId } = useParams();
 
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        dispatch(getGymDetail(userId))// eslint-disable-next-line
-    },[userId])
+  const dispatch = useDispatch();
 
-   
+  useEffect(() => {
+    dispatch(getGymDetail(userId)); // eslint-disable-next-line
+  }, [userId]);
 
-    const gymDetail = useSelector((state) => state.gymDetail)
-    
-    let usuarioId = localStorage.getItem('userId')
-    // id de usuario que está en la app
-    // console.log(gymDetail, 'id de usuario que está en la app')
+  const gymDetail = useSelector((state) => state.gymDetail);
 
+  let usuarioId = localStorage.getItem("userId");
+  // id de usuario que está en la app
+  // console.log(gymDetail, 'id de usuario que está en la app')
 
-    if (!gymDetail.name) {
-        return (
-            <img id='loading' src="https://www.sanfranciscohm.com/static/img/loading.gif" alt="loading..." />                           
-        )
+  if (!gymDetail.name) {
+    return (
+      <img
+        id="loading"
+        src="https://www.sanfranciscohm.com/static/img/loading.gif"
+        alt="loading..."
+      />
+    );
+  } else {
+    return (
+      <div>
+        {/* Bloque de titulo */}
+        <section className={style.curved}>
+          <p className={style.title}>{gymDetail.name}</p>
+        </section>
+        {/* Bloque de cards presentacion */}
+        <div className={style.contDat}>
+          <CardIcons img={start} num={gymDetail.raiting} />
+          <CardIcons img={user} num="55" />
+          <CardIcons img={map} num="1.4 Km" />
+          <CardIcons img={actividades} num={gymDetail.services.length} />
+        </div>
         
-    } else {
-        return (
-            <div>
-                <h4 style={{color: "#fff"}}>Id del usuario que navega: {usuarioId ? usuarioId: null}</h4>
-                <h2>{gymDetail.name}</h2>
-                <h3> ☆ {gymDetail.raiting}</h3>
-                <h3> $ {gymDetail.price.$numberDecimal}</h3>
-                <h3 id={gymDetail._id} ></h3> 
-                {/* <h3>{console.log(gymDetail._id)}</h3> */}
-                <div>                        
-                    <img src={gymDetail.image[1]} alt="logo" />
-                </div>
-                <div>
-                <NavBar3 id={[gymDetail]} usuarioId={usuarioId}/>
-                </div>
-                <div>
-                    {gymDetail.services.map(e => {
-                        return (
-                            <div  key={e._id}> 
-                            <CartItem id={e._id} key={e._id} name={e.name} price={e.price} description={e.description} duration={e.duration} />                            <br></br>
-                            </div>
-                        )
-                    })}
-                </div>
-                <button onClick={() => navigate(-1)}>Go back</button>
-            </div>        
-        )
+        {/* Bloque de info del carrito */}
+        <div>
+          <NavBar3 id={[gymDetail]} usuarioId={usuarioId} />
+        </div>
 
-    }
-
-};
+        {/* Detalle servicio */}
+        <div className={style.contServices}>
+          {gymDetail.services.map((e) => {
+            return (
+              <div key={e._id}>
+                <CartItem
+                  id={e._id}
+                  key={e._id}
+                  name={e.name}
+                  price={e.price}
+                  description={e.description}
+                  duration={e.duration}
+                />{" "}
+                <br></br>
+              </div>
+            );
+          })}
+        </div>
+        <button onClick={() => navigate(-1)}>Go back</button>
+      </div>
+    );
+  }
+}
 
 //http://localhost:3000/detail/gym/6292dae93fc1e9d735aea34c
-// 
+//
