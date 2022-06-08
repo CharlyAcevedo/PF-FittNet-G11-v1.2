a12e68c3e7665115df415e87d29bb32c73953692
nano0144 <fer_0144@hotmail.com>
2022-06-08 18:42:58 -0300

    add other view to admin

--- client/src/components/Home/HomeAdmin/HomeAdmin.jsx
+++ client/src/components/Home/HomeAdmin/HomeAdmin.jsx
@@ -4,6 +4,7 @@ import Users from "./ViewsAdmin/Users";
 import ViewPartner from "./ViewsAdmin/ViewPartner";
 import ViewUsers from "./ViewsAdmin/ViewUser";
 import DeteleteAccount from "./ViewsAdmin/DeleteAccount";
+import OtherView from "./ViewsAdmin/OtherView";
 
 
 import React from "react";
@@ -13,179 +14,36 @@ export default function HomeAdmin() {
     const [ view , setView ] = useState("");
 
     // Necesito una barra de búsqueda con filtros
-
-    if(view === "finances" ){ // ver finanzas
-        return(
-            <div style={{padding: "20px", color: "#fff" }}>
-                <button style={{padding: "5px", marginRight:"30px"}} 
-                onClick={(e)=>{setView("")}}>Pendientes</button>
-                <button style={{padding: "5px", marginRight:"30px"}} 
-                onClick={(e)=>{setView("finances")}}>Finanzas</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("partners")}}>Partner</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("users")}}>Users</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("viewPartner")}}>Vista Partner</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("viewUser")}}>Vista User</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("deleteAccount")}}>Eliminar cuenta</button>
-                
-                <p style={{padding: "10px"}}>Necesito información financiera, números</p>
-                <Finances/>
-            </div>
-        )
-
-    }
-    if(view === "partners" ){ // clientes empresa
-        return(
-            <div style={{padding: "20px", color: "#fff" }}>
-                <button style={{padding: "5px", marginRight:"30px"}} 
-                onClick={(e)=>{setView("")}}>Pendientes</button>
-                <button style={{padding: "5px", marginRight:"30px"}} 
-                onClick={(e)=>{setView("finances")}}>Finanzas</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("partners")}}>Partner</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("users")}}>Users</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("viewPartner")}}>Vista Partner</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("viewUser")}}>Vista User</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("deleteAccount")}}>Eliminar cuenta</button>
-                
-                <p style={{padding: "10px"}}>Necesito cargar la info del estado de partners, una una barra de búsqueda y filtros</p>
-                <Partners/>
-            </div>
-        )
-        
-    }
-    if(view === "users" ){ // usuario finales
-        return(
-            <div style={{padding: "20px", color: "#fff" }}>
-                <button style={{padding: "5px", marginRight:"30px"}} 
-                onClick={(e)=>{setView("")}}>Pendientes</button>
-                <button style={{padding: "5px", marginRight:"30px"}} 
-                onClick={(e)=>{setView("finances")}}>Finanzas</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("partners")}}>Partner</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("users")}}>Users</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("viewPartner")}}>Vista Partner</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("viewUser")}}>Vista User</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("deleteAccount")}}>Eliminar cuenta</button>
-                
-                <p style={{padding: "10px"}}>Necesito una lista de todos los usuarios finales adeheridos, una una barra de búsqueda y filtros</p>
-                <Users/>
-            </div>
-        )
-        
-    }
-    if(view === "deleteAccount" ){ // Eliminar o banear usuario partener o user
-        return(
-            <div style={{padding: "20px", color: "#fff" }}>
-                <button style={{padding: "5px", marginRight:"30px"}} 
-                onClick={(e)=>{setView("")}}>Pendientes</button>
-                <button style={{padding: "5px", marginRight:"30px"}} 
-                onClick={(e)=>{setView("finances")}}>Finanzas</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("partners")}}>Partner</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("users")}}>Users</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("viewPartner")}}>Vista Partner</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("viewUser")}}>Vista User</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("deleteAccount")}}>Eliminar cuenta</button>
-                
-                <DeteleteAccount/>
-            </div>
-        )
-        
-    }
-    if(view === "viewPartner" ){ // Vista como partner
-        return(
-            <div style={{padding: "20px", color: "#fff" }}>
-                <button style={{padding: "5px", marginRight:"30px"}} 
-                onClick={(e)=>{setView("")}}>Pendientes</button>
-                <button style={{padding: "5px", marginRight:"30px"}} 
-                onClick={(e)=>{setView("finances")}}>Finanzas</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("partners")}}>Partner</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("users")}}>Users</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("viewPartner")}}>Vista Partner</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("viewUser")}}>Vista User</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("deleteAccount")}}>Eliminar cuenta</button>
-                               
-                <ViewPartner/>
-            </div>
-        )
-        
-    }
-    if(view === "viewUser" ){ // Vista como user
-        return(
-            <div style={{padding: "20px", color: "#fff" }}>
-                <button style={{padding: "5px", marginRight:"30px"}} 
-                onClick={(e)=>{setView("")}}>Pendientes</button>
-                <button style={{padding: "5px", marginRight:"30px"}} 
-                onClick={(e)=>{setView("finances")}}>Finanzas</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("partners")}}>Partner</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("users")}}>Users</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("viewPartner")}}>Vista Partner</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("viewUser")}}>Vista User</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("deleteAccount")}}>Eliminar cuenta</button>
-                               
-                <ViewUsers/>
-            </div>
-        )
-        
-    }
     
-    if(view === "" ){ // Otra vista
-        return(
-            <div style={{padding: "20px", color: "#fff" }}>
-                <button style={{padding: "5px", marginRight:"30px"}} 
-                onClick={(e)=>{setView("")}}>Pendientes</button>
-                <button style={{padding: "5px", marginRight:"30px"}} 
-                onClick={(e)=>{setView("finances")}}>Finanzas</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("partners")}}>Partner</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("users")}}>Users</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("viewPartner")}}>Vista Partner</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("viewUser")}}>Vista User</button>
-                <button style={{padding: "5px", marginRight:"30px"}}
-                onClick={(e)=>{setView("deleteAccount")}}>Eliminar cuenta</button>
-
-                <p style={{padding: "10px"}}>Quiero una pestaña dinámica para pasar de un tema al otro</p>
-                <p style={{padding: "10px"}}></p>
-                <p style={{padding: "10px"}}>¿qué vamos a mostrar en esta vista para el admin?</p>                
-                <p style={{padding: "10px"}}>1 - Necesito información financiera, números</p>
-                <p style={{padding: "10px"}}>2 - Necesito una lista de todos de todos los clientes empresa adheridos</p>
-                <p style={{padding: "10px"}}>3 - Necesito una lista de todos los usuarios finales adeheridos</p>
-                <p style={{padding: "10px"}}>4 - Necesito poder eliminar una cuenta de partner o user</p>
-                <p style={{padding: "10px"}}>5 - Necesito una vista como user</p>
-                <p style={{padding: "10px"}}>6 - Necesito una vista como partner</p>            
-            </div>
-        )
-    }
+    return(
+        <div style={{padding: "20px", color: "#fff" }}>
+            <button style={{padding: "5px", marginRight:"30px"}} 
+            onClick={(e)=>{setView("")}}>Pendientes</button>
+            <button style={{padding: "5px", marginRight:"30px"}} 
+            onClick={(e)=>{setView("finances")}}>Finanzas</button>
+            <button style={{padding: "5px", marginRight:"30px"}}
+            onClick={(e)=>{setView("partners")}}>Partner</button>
+            <button style={{padding: "5px", marginRight:"30px"}}
+            onClick={(e)=>{setView("users")}}>Users</button>
+            <button style={{padding: "5px", marginRight:"30px"}}
+            onClick={(e)=>{setView("viewPartner")}}>Vista Partner</button>
+            <button style={{padding: "5px", marginRight:"30px"}}
+            onClick={(e)=>{setView("viewUser")}}>Vista User</button>
+            <button style={{padding: "5px", marginRight:"30px"}}
+            onClick={(e)=>{setView("deleteAccount")}}>Eliminar cuenta</button>
+            
+            <p style={{padding: "10px"}}>Necesito información financiera, números</p>
+            {view === "finances" && <Finances/>}
+            {view === "partners" && <Partners/>}
+            {view === "users" && <Users/>}
+            {view === "viewPartner" && <ViewPartner/>}
+            {view === "viewUser" &&  <ViewUsers/>}
+            {view === "deleteAccount" &&  <DeteleteAccount/>}
+            {view === "" && <OtherView/> }
+            
+        </div>
+    )
+    
 
         
 }
