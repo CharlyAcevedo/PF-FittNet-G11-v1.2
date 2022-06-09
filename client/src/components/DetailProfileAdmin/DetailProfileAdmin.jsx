04eb73af2797398efdd8c11ce1d003c0afdad263
nano0144 <fer_0144@hotmail.com>
2022-06-09 11:26:28 -0300

    agreo más funciones al prefil admin en su home

--- client/src/components/DetailProfileAdmin/DetailProfileAdmin.jsx
+++ client/src/components/DetailProfileAdmin/DetailProfileAdmin.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAdmin } from "../../redux/actions";
+
 
 export default function DetailProfileAdmin() {
     let { userId , name, type } = useParams();
 
     const dispatch = useDispatch();
 
-    useEffect(() => {
-        console.log('useEffect')
+    const user = useSelector((state) => state.user);
 
-        if (userId.length > 20) {
-            console.log('sale la cción', userId)
-            
+    useEffect(() => {       
+        if (userId.length > 20) {          
             dispatch(getAdmin(userId)); 
         }      
-       
-    });  
+    },[userId]);  
 
     // con el id ya podemos solicitar info a nuestro back, el cual solo responderá
     // si le llega este id (de la fomra que lo espera) y si el usuario tiene una
@@ -33,14 +31,15 @@ export default function DetailProfileAdmin() {
         <div style={{padding: "20px", color: "#fff" }}> 
             
             <h3 style={{padding: "15px"}}>Perfil del admin</h3>
+            <p style={{padding: "5px"}}>Quiero mostrar toda su infomación</p>
            
-            <p style={{padding: "5px"}}>Lo que tengo por params</p>
-            <p style={{padding: "5px"}}>Typo: {type}, name: {name}, userId: {userId}</p>
+            <p style={{padding: "5px"}}>Nombre: {user.name && user.name}</p>
+            <p style={{padding: "5px"}}>Email: {user.userName && user.userName}</p>
+            
             <p style={{padding: "15px"}}></p> 
            
 
             <h3 style={{padding: "5px"}}>Detalles del perfil admin</h3>
-            <p style={{padding: "5px"}}>Quiero mostrar toda su infomación</p>
 
             <p style={{padding: "5px"}}></p>         
             <h3 style={{padding: "5px"}}>Cosas que necesito</h3>
@@ -48,7 +47,9 @@ export default function DetailProfileAdmin() {
             <p style={{padding: "5px"}}>2 - Neceisto ver la estructura de esa infomación</p>
             <p style={{padding: "5px"}}>3 - Necesito una foto del admin</p>
             <p style={{padding: "5px"}}>4 - Necesito que esta sección tenga el mismo estilo que la sección de perfil user</p>
-            <p style={{padding: "15px"}}></p> 
+            <p style={{padding: "15px"}}></p>
+            <p style={{padding: "5px"}}>Id: {user._id && user._id}</p> 
+            <p style={{padding: "15px"}}></p>
            
             <a style={{padding: "5px"}}href={`/home/editprofile/${type}/${name}/${userId}`}>Editar mi perfil</a>
            
