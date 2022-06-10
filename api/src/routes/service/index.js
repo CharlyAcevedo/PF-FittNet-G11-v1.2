b74c49a2ef8fd87342c3c878c36515485d5708cc
nano0144 <fer_0144@hotmail.com>
2022-06-10 11:07:57 -0300

    se arregla un conflicto

--- api/src/routes/service/index.js
+++ api/src/routes/service/index.js
@@ -6,11 +6,9 @@ const routeResetPassword = require('./updatePass');
 const routeActivation = require('./activation');
 const routerEmailValidate = require('./emailValidate');
 const routerGoogleAuth = require('./googleAuth');
-<<<<<<< HEAD
-const routeEmails = require('./emails');
-=======
 const routerMarketing = require('./marketing');
->>>>>>> 7cf7156f883f8d293575fde7a636a4097de773ff
+const routeEmails = require('./emails');
+
 
 router.use('/', routeLogin); //ruta /api/service/login get y post
 router.use('/', routeLogout); //ruta /api/service/logout post
@@ -19,11 +17,9 @@ router.use('/', routeResetPassword); //ruta /api/service/updatepassword post
 router.use('/', routeActivation); //ruta /api/service/activation//:userId/:secretToken get y /api/service/deleteuseraccount put (solo desactiva la cuenta)
 router.use('/', routerEmailValidate) //ruta /api/service/logout post
 router.use('/', routerGoogleAuth) //ruta /api/service/google/auth post
-<<<<<<< HEAD
-router.use('/', routeEmails); //ruta /api/service/emails --> para enviar correos
-=======
 router.use('/', routerMarketing) //ruta /api/service/
->>>>>>> 7cf7156f883f8d293575fde7a636a4097de773ff
+router.use('/', routeEmails); //ruta /api/service/emails --> para enviar correos
+
 
 router.get('/', async (req, res) => {
     try {
