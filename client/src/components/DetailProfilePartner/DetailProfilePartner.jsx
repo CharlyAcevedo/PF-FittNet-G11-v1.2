1e9b16251299566c35a227b37cea84294584c2f5
nano0144 <fer_0144@hotmail.com>
2022-06-10 21:11:23 -0300

    avance del home del partner

--- client/src/components/DetailProfilePartner/DetailProfilePartner.jsx
+++ client/src/components/DetailProfilePartner/DetailProfilePartner.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import ClientsGraph from "../Graphics/GraphClient";
-import IncomesGraph from "../Graphics/Incomes";
+
 import styles from "./styles/DetailProfilePartner.module.css";
 
 export default function DetailProfilePartner() {
-  let { id, userId, name, type } = useParams();
-  // console.log(id, name, type, 'id y name')
+  let { userId, name, type } = useParams();
+  console.log(userId, name, type, 'id y name')
 
   // con el id ya podemos solicitar info a nuestro back, el cual solo responderá
   // si le llega este id (de la fomra que lo espera) y si el usuario tiene una
@@ -14,78 +13,46 @@ export default function DetailProfilePartner() {
 
   // Queda pendiente dispachar una acción para cargar en el estado global
   // la info del usuario en un objeto. Ejeplo --> State.detailUser = {}
-
+    
   return (
     <div className={styles.partnerMainContainer}>
-      {/* <NavBarProfile/>  */}
-      <p>Id: {id}, name: {name} </p>
-            <p>Typo: {type}</p>            
-      <br />
+      <div className={styles.partnerMiniContainer}>
+        <p>userId: {userId} </p>
+        <p>name: {name} </p>
+        <p>Typo: {type}</p>
+      </div>
 
+      <div className={styles.partnerMiniContainer}>
+        <h3>Detalles de su perfil</h3>
+        <p>En esta seccion usted podrá ver la informacion de su perfil</p>
+        <p>Nombre: {name}</p>
+        <p>Apellido: {null}</p>
+        <p>Email: {null}</p>
+        <p>Telefono: {null}</p>
+        <p>Tipo de plan: {null}</p>
+        <p></p>
+        <p>CBU: {null}</p>
+        <p>Perfil: {null}</p>
+        <p>Usuario activo: {null}</p>
+        <p>Redes Sociales: {null}</p>
+        <p>Metodos de Pago: {null}</p>
+        <p>Categoria: {null}</p>
+        <p></p>
+      </div>
 
-      <section className={styles.partnerTitles}>
-        <h3>Perfil de Partner</h3>
-        <p>
-          En esta seccion usted podrá ver la informacion de su perfil;
-          Información sobre sus Ventas, su Flujo de fondos y mas
-        </p>
-      </section>       
-      <section className={styles.partnerIncomes}>
-        <section className={styles.partnerSales}>
-          <h3>Gestión de ventas</h3>
-          <p>Mis ventas e historial de ventas</p>
-          <IncomesGraph />
-        </section>
-        <section className={styles.partnerCashFlow}>
-          <h3>Flujo de fondos</h3>
-          <p>Mi caja</p>
-          <br />
-        </section>
-      </section>
-      <section className={styles.partnerClients}>
-        <h3>Mis Clientes</h3>
-        <ClientsGraph />
-      </section>
-      <section className={styles.partnerFinalSection}>
-        <section className={styles.partnerProfileDetails}>
-          <h3>Detalles del perfil</h3>
-          <p>Name: {name}</p>
-          <br />
-          <p>Tipo de plan: estandar, premium o golden</p>
-          <br />
-        </section>
-        <section className={styles.partnerLinks}>
-          <a
-            style={{ color: "#fff" }}
-            href={`/profile/edit/${type}/${name}/${userId}`}
-          >
-            Editar mi perfil
-          </a>
-          <br />
-          <br />
-          <a
-            style={{ color: "#fff" }}
-            href={`/profile/edit/${type}/${name}/${userId}/gym`}
-          >
-            Editar mis gimnasios
-          </a>
-          <br />
-          <br />
-          <a style={{ color: "#fff" }} href={`/updatepassword/${userId}`}>
-            Cambiar mi contraseña
-          </a>
-          <br />
-          <br />
-          <a style={{ color: "#fff" }} href={`/deactivate/${userId}`}>
-            Borra mi cuenta
-          </a>
-          <br />
-          <br />
-          <a style={{ color: "#fff" }} href="/">
-            Volver
-          </a>
-        </section>
-      </section>
+      <div className={styles.partnerMiniContainer}>
+        <a style={{ paddingRight: "20px" }}
+          href={`/profile/edit/${type}/${name}/${userId}`}>Editar mi perfil</a>
+        <a style={{ paddingRight: "20px" }}
+        //   href={`/profile/edit/${type}/${name}/${userId}/gym`}>Editar mis gimnasios </a>
+        // <a style={{ paddingRight: "20px" }}
+          href={`/updatepassword/${userId}`}>Cambiar mi contraseña</a>
+        <a style={{ paddingRight: "20px" }}
+          href={`/deactivate/${userId}`}>Borra mi cuenta</a>
+        <a style={{ paddingRight: "20px" }} href="/">
+          Volver
+        </a>
+      </div>
     </div>
   );
 }
